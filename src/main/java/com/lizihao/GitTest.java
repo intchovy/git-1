71d9b2e3ad66a9a50cb400b515b2946cee49f052
liziihao <413766428@qq.com>
2022-07-13 18:09:03 +0800

    ...

--- src/main/java/com/lizihao/GitTest.java
+++ src/main/java/com/lizihao/GitTest.java
@@ -3,8 +3,7 @@ package com.lizihao;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("好的");
-        System.out.println("好的好的");
-        System.out.println("好的好的好的");
+
 
     }
 }
